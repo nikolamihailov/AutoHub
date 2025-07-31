f3fa1e11cb0ec470ec1a8e2ea5b20fd2e9238fca
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-07-31 18:49:54 +0300

    change folder structure and create basic pages

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,37 +1,40 @@
 import { Routes } from '@angular/router';
+import { ErrorPage, Home } from './pages';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
   /*  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'logout', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'profile', component: ProfileComponent },
-  {
-    path: 'car-offers',
-    children: [
-      { path: '', component: CarOffers },
-      { path: 'add', component: CarOfferAddComponent },
-      { path: 'edit', component: CarOfferEditComponent },
-      { path: 'saved', component: CarOffersSavedComponent },
-      { path: 'user/:userId', component: CarOffersUserComponent },
-      { path: ':id', component: CarOfferComponent },
-    ],
-  },
-  {
-    path: 'categories',
-    children: [
-      { path: '', component: CategoriesComponent },
-      { path: ':id', component: CategoryComponent },
-    ],
-  },
-  {
-    path: 'admin-dashboard',
-    children: [
-      { path: '', component: AdminDashboardComponent },
-      { path: 'users', component: AdminUsersComponent },
-      { path: 'categories', component: AdminCategoriesComponent },
-      { path: 'car-offers', component: AdminCarOffersComponent },
-    ],
-  }, */
+    { path: 'register', component: RegisterComponent },
+    { path: 'logout', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'profile', component: ProfileComponent },
+    {
+      path: 'car-offers',
+      children: [
+        { path: '', component: CarOffers },
+        { path: 'add', component: CarOfferAddComponent },
+        { path: 'edit', component: CarOfferEditComponent },
+        { path: 'saved', component: CarOffersSavedComponent },
+        { path: 'user/:userId', component: CarOffersUserComponent },
+        { path: ':id', component: CarOfferComponent },
+      ],
+    },
+    {
+      path: 'categories',
+      children: [
+        { path: '', component: CategoriesComponent },
+        { path: ':id', component: CategoryComponent },
+      ],
+    },
+    {
+      path: 'admin-dashboard',
+      children: [
+        { path: '', component: AdminDashboardComponent },
+        { path: 'users', component: AdminUsersComponent },
+        { path: 'categories', component: AdminCategoriesComponent },
+        { path: 'car-offers', component: AdminCarOffersComponent },
+      ],
+    }, */
+  { path: 'not-found', component: ErrorPage },
   { path: '**', redirectTo: 'not-found' },
 ];
---
+++ client/src/app/pages/index.ts
@@ -0,0 +1,2 @@
+export * from './home/home';
+export * from './error-page/error-page';
