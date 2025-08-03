80352b77d74be0d10ce9fe93f076b17e45579aa1
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-08-04 00:43:50 +0300

    update to new version of header

--- client/src/app/shared/components/header/header.ts
+++ client/src/app/shared/components/header/header.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Logo } from '../logo/logo';
 import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
-  imports: [Logo, RouterLink],
+  imports: [Logo, RouterLink, MatIconModule, MatMenuModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
