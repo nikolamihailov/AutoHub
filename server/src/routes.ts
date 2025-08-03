43c32783fe4c76ef1a9075841258e4d1b9884b7f
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-08-04 02:42:30 +0300

    include users controller in the router

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { carsController } from './controllers/carController';
+import { userController } from './controllers/userController';
 
 const router = Router();
 
+router.use('/users', userController);
 router.use('/cars', carsController);
 
 export { router };
