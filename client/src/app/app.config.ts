f353f9a1d14bd3dc3a2b22a8a6cc7edf4ba03ef9
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-08-04 00:45:15 +0300

    update config to use toast

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import {
   ApplicationConfig,
+  importProvidersFrom,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
@@ -8,11 +9,20 @@ import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
     provideHttpClient(),
+    importProvidersFrom(BrowserAnimationsModule),
+    importProvidersFrom(
+      ToastrModule.forRoot({
+        positionClass: 'toast-top-right',
+      }),
+    ),
   ],
 };
