aa2a9e00b0d0a43c2c46b35aa75be0acc0e51da0
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-08-05 13:15:53 +0300

    change user model

--- client/src/app/models/user.model.ts
+++ client/src/app/models/user.model.ts
@@ -4,7 +4,7 @@ export interface User {
   lastName: string;
   email: string;
   password: string;
-  avatar: string;
+  avatar: string | undefined;
   role: Role;
   accounType: AccountType;
   // savedCarOffers: CarOffer[];
