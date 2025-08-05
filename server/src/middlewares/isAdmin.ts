f6f71447769410c3411becd2acff4a07ecd8c180
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-08-06 02:53:49 +0300

    add new enum and fix broken mdw

--- server/src/middlewares/isAdmin.ts
+++ server/src/middlewares/isAdmin.ts
@@ -1,27 +1,26 @@
 import { Request, Response, NextFunction } from 'express';
 import { Role } from '../models/User.model';
 
-interface MyRequest extends Request {
-  user?: {
-    data: {
-      user: {
-        _id: string;
-        role: Role;
+export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+  const myReq = req as Request & {
+    user?: {
+      data?: {
+        user?: {
+          _id: string;
+          email: string;
+          role: Role;
+        };
       };
     };
   };
-  decToken?: any;
-}
 
-export const isAdmin = (req: MyRequest, res: Response, next: NextFunction) => {
-  if (req.user?.data.user && req.user?.data.user.role === Role.ADMIN) {
-    next();
-  } else {
-    if (req.decToken) {
-      return res
-        .status(401)
-        .json({ expMessage: 'Your session has expired, you have to login again!' });
-    }
-    res.status(401).json({ unauthorized: 'You have no power here!' });
+  if (
+    myReq.user &&
+    myReq.user.data &&
+    myReq.user.data.user &&
+    myReq.user.data.user.role === Role.ADMIN
+  ) {
+    return next();
   }
+  return res.status(401).json({ unauthorized: 'You have no power here!' });
 };
