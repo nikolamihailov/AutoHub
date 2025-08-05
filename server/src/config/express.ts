ed9a569aec10d110c8a7ade4d76e01f3f980efed
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-08-05 14:33:45 +0300

    fix default limit

--- server/src/config/express.ts
+++ server/src/config/express.ts
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 5173;
 export const startServer = () => {
   const app = express();
 
-  app.use(express.json());
+  app.use(express.json({ limit: '5mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '5mb' }));
   app.use(cors());
 
   app.use(router);
