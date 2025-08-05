17e18b766bd0cb4396953e21291241968a23d203
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-08-05 13:13:55 +0300

    improve header with new links and admin checks

--- client/src/app/shared/components/header/header.ts
+++ client/src/app/shared/components/header/header.ts
@@ -1,20 +1,48 @@
 import { Component, inject } from '@angular/core';
 import { Logo } from '../logo/logo';
-import { RouterLink } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { AuthService } from '../../../core/services/user/authService.service';
+import { ToastrService } from 'ngx-toastr';
+import { Role } from '../../../models';
 
 @Component({
   selector: 'app-header',
-  imports: [Logo, RouterLink, MatIconModule, MatMenuModule],
+  imports: [Logo, RouterModule, MatIconModule, MatMenuModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
   protected authService = inject(AuthService);
+  protected router = inject(Router);
+  protected toast = inject(ToastrService);
 
-  get isLoggedIn() {
+  protected showAdminNav = false;
+  protected currentUser = this.authService.currentUser();
+
+  isLoggedIn() {
     return this.authService.isLoggedIn();
+  }
+
+  isAdmin(): boolean {
+    const user = this.authService.currentUser();
+    return !!user && user.role === Role.ADMIN;
+  }
+
+  handleLogout() {
+    this.authService.logout();
+    this.router.navigate(['/logout']);
+    this.toast.success('You logged out!');
+  }
+
+  handleAdminPanelEnter() {
+    this.showAdminNav = true;
+    this.router.navigate(['/admin-dashboard']);
+  }
+
+  handleAdminPanelLeave() {
+    this.showAdminNav = false;
+    this.router.navigate(['/home']);
   }
 }
