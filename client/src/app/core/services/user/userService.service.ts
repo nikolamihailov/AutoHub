7cab1f06e2a0dae2ab02cef6e0bedd02ba4450f5
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-08-06 02:57:30 +0300

    bump new methods for delete and getallpaginated with sort

--- client/src/app/core/services/user/userService.service.ts
+++ client/src/app/core/services/user/userService.service.ts
@@ -1,12 +1,12 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environments';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { AuthService } from './authService.service';
 import { Observable, throwError } from 'rxjs';
-import { User } from '../../../models';
+import { PaginatedUsersResponse, Sort, User } from '../../../models';
 
 @Injectable({ providedIn: 'root' })
-export class Userservice {
+export class UserService {
   private BASE_URL = environment.BASE_URL;
   private authService = inject(AuthService);
 
@@ -26,5 +26,24 @@ export class Userservice {
       return throwError(() => new Error('No user logged in'));
     }
     return this.http.put<User>(`${this.BASE_URL}/users/${currentUser._id}`, data);
+  }
+
+  public getUsers(
+    limit?: string,
+    page?: string,
+    searchTerm?: string,
+    sort?: Sort | '',
+  ): Observable<PaginatedUsersResponse> {
+    let params = new HttpParams();
+    if (limit) params = params.set('limit', limit);
+    if (page) params = params.set('page', page);
+    if (searchTerm) params = params.set('searchTerm', searchTerm);
+    if (sort) params = params.set('sort', sort);
+
+    return this.http.get<PaginatedUsersResponse>(`${this.BASE_URL}/users`, { params });
+  }
+
+  public deleteUser(id: string): Observable<User> {
+    return this.http.delete<User>(`${this.BASE_URL}/users/${id}`);
   }
 }
