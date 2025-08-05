8c32cb2c948133362bfe9861e3ab805dc3bbb48c
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-08-05 13:05:06 +0300

    add new header allowed

--- server/src/middlewares/cors.ts
+++ server/src/middlewares/cors.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 export const cors = () => (req: Request, res: Response, next: NextFunction) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, *');
 
   if (req.method === 'OPTIONS') {
     res.status(200).end();
