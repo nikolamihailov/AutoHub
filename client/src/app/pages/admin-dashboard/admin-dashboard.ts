aafe506b51e99cb6e0d44071cd21d5c63542d2d9
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-08-08 00:23:29 +0300

    use admin heaer in default route for admin dashboard

--- client/src/app/pages/admin-dashboard/admin-dashboard.ts
+++ client/src/app/pages/admin-dashboard/admin-dashboard.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { AdminPageHeader } from '../../shared/components';
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [],
+  imports: [AdminPageHeader],
   templateUrl: './admin-dashboard.html',
-  styleUrl: './admin-dashboard.scss'
+  styleUrl: './admin-dashboard.scss',
 })
-export class AdminDashboard {
-
-}
+export class AdminDashboard {}
