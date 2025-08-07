3c8dc65473cb3b7d08549f13907ab581c9ef6b7b
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-08-07 04:23:55 +0300

    admin categories with new header

--- client/src/app/pages/admin-categories/admin-categories.ts
+++ client/src/app/pages/admin-categories/admin-categories.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CategoriesManagement } from '../../features';
+import { AdminPageHeader } from '../../shared/components';
 
 @Component({
   selector: 'app-admin-categories',
-  imports: [],
+  imports: [CategoriesManagement, AdminPageHeader],
   templateUrl: './admin-categories.html',
-  styleUrl: './admin-categories.scss'
+  styleUrl: './admin-categories.scss',
 })
-export class AdminCategories {
-
-}
+export class AdminCategories {}
