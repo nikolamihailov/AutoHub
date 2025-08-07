e843d7c718ab01b14faf3e99a5b475bff73ed0e1
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-08-07 04:19:40 +0300

    add new header for admin users info

--- client/src/app/pages/admin-users/admin-users.ts
+++ client/src/app/pages/admin-users/admin-users.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { UsersInfo } from '../../features/user/admin/users-info/users-info';
+import { UsersInfo } from '../../features';
+import { AdminPageHeader } from '../../shared/components';
 
 @Component({
   selector: 'app-admin-users',
-  imports: [UsersInfo],
+  imports: [UsersInfo, AdminPageHeader],
   templateUrl: './admin-users.html',
   styleUrl: './admin-users.scss',
 })
