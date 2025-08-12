1de08dec53a39e579a70b675d11ef304820ccd3c
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-08-12 03:03:54 +0300

    add new method to service

--- client/src/app/core/services/car-offer/carOfferService.service.ts
+++ client/src/app/core/services/car-offer/carOfferService.service.ts
@@ -3,6 +3,8 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environments';
 import { CarOffer } from '../../../models';
+import { PaginatedCarOffersResponse } from '../../../models/car-offerResponse';
+import { Sort } from '../../../shared/enums/Sort.enum';
 
 @Injectable({ providedIn: 'root' })
 export class CarOfferService {
@@ -12,5 +14,22 @@ export class CarOfferService {
 
   public createCategory(data: Omit<CarOffer, '_id'>): Observable<CarOffer> {
     return this.http.post<CarOffer>(`${this.BASE_URL}/car-offers`, data);
+  }
+
+  public getCarOffers(
+    limit?: string,
+    page?: string,
+    searchTerm?: string,
+    sort?: Sort | '',
+  ): Observable<PaginatedCarOffersResponse> {
+    let params = new HttpParams();
+    if (limit) params = params.set('limit', limit);
+    if (page) params = params.set('page', page);
+    if (searchTerm) params = params.set('searchTerm', searchTerm);
+    if (sort) params = params.set('sort', sort);
+
+    return this.http.get<PaginatedCarOffersResponse>(`${this.BASE_URL}/car-offers`, {
+      params,
+    });
   }
 }
