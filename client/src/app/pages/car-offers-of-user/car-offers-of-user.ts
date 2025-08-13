6c64172620eee23e43dc4f65242ccd6cebab2e7a
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-08-13 22:48:52 +0300

    add back btn for all of user cars

--- client/src/app/pages/car-offers-of-user/car-offers-of-user.ts
+++ client/src/app/pages/car-offers-of-user/car-offers-of-user.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CarOffersForUser } from '../../features';
+import { BackBtn } from '../../shared/components';
 
 @Component({
   selector: 'app-car-offers-of-user',
-  imports: [CarOffersForUser],
+  imports: [CarOffersForUser, BackBtn],
   templateUrl: './car-offers-of-user.html',
   styleUrl: './car-offers-of-user.scss',
 })
