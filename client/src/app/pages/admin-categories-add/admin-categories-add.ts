4021908d90f5613c289862b72171ba1f7602cb71
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2025-08-13 23:01:22 +0300

    add back btn to admin categories form

--- client/src/app/pages/admin-categories-add/admin-categories-add.ts
+++ client/src/app/pages/admin-categories-add/admin-categories-add.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CategoryAddForm } from '../../features';
+import { BackBtn } from '../../shared/components';
 
 @Component({
   selector: 'app-admin-categories-add',
-  imports: [CategoryAddForm],
+  imports: [CategoryAddForm, BackBtn],
   templateUrl: './admin-categories-add.html',
   styleUrl: './admin-categories-add.scss',
 })
--- client/src/app/pages/admin-categories-edit/admin-categories-edit.ts
+++ client/src/app/pages/admin-categories-edit/admin-categories-edit.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CategoryEditForm } from '../../features';
+import { BackBtn } from '../../shared/components';
 
 @Component({
   selector: 'app-admin-categories-edit',
-  imports: [CategoryEditForm],
+  imports: [CategoryEditForm, BackBtn],
   templateUrl: './admin-categories-edit.html',
   styleUrl: './admin-categories-edit.scss',
 })
